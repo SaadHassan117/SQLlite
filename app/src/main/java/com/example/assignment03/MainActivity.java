98651dae94cc717feda2cf582e1ec1988239f70f
SaadHassan117 <saadh9290@gmail.com>
2022-01-15 08:12:16 -0700

    add student

--- app/src/main/java/com/example/assignment03/MainActivity.java
+++ app/src/main/java/com/example/assignment03/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button add, show;
@@ -34,12 +37,22 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Student obj =new Student(name.getText().toString(),Integer.parseInt(id.getText().toString()),actives.isChecked());
                          Toast.makeText(MainActivity.this,obj.toString(),Toast.LENGTH_SHORT).show();
-
+                    DBHelper dbHelper = new DBHelper(MainActivity.this);
+                    dbHelper.addStudent(obj);
                 }
                 catch(Exception obj)
                 {
                     Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                List<Student> liste = dbHelper.getAllStudents();
+                ArrayAdapter arrayAdapter = new ArrayAdapter<Student>(MainActivity.this, android.R.layout.simple_list_item_1,liste);
+                list.setAdapter(arrayAdapter);
             }
         });
     }
